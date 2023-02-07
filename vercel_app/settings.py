880fb35009a8ada5aa7e8246a0926f326676c487
Mehdi Khoudali <mehdikhoudalpro@gmail.com>
2023-02-07 19:58:36 +0100

    finished the navbar

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = 'django-insecure-=cldztbc4jg&xl0!x673!*v2_=p$$eu)=7*f#d0#zs$44xx-h^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app']
 
 INSTALLED_APPS = [
     'django.contrib.admin',

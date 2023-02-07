01b2715125c89f01cf5158bb1b731250b28f0e06
Mehdi Khoudali <mehdikhoudalpro@gmail.com>
2023-02-07 18:06:12 +0100

    dhqskhdqslhd

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -38,7 +38,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'vercel_app.urls'

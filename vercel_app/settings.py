71d52591e623819fe3cde83fe9a597386b1d87c2
Mehdi Khoudali <mehdikhoudalpro@gmail.com>
2023-02-11 21:33:58 +0100

    corrected the db

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -48,16 +48,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'vercel_app.wsgi.app'
 
-DATABASES = {
-       'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': '',
-       'USER': 'postgres',
-       'PASSWORD': 'q4ZyaMQQ2j58HS8Nh6ke',
-       'HOST': 'containers-us-west-197.railway.app',
-       'PORT': '6873',
-   }
-}
+DATABASES = {}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
